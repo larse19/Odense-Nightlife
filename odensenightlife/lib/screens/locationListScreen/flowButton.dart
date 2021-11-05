4fa02826f6faae47aa959e46c42a270d5c39aa10
Theis <theistengs@gmail.com>
2021-11-05 13:38:39 +0100

    Flow button added to locationScreen
    Still needs to do stuff when pressed

---
+++ odensenightlife/lib/screens/locationListScreen/flowButton.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+const double buttonSize = 80;
+
+class FlowMenu extends StatefulWidget {
+  const FlowMenu({Key? key}) : super(key: key);
+
+  @override
+  State<FlowMenu> createState() => _FlowMenuState();
+}
+
+class _FlowMenuState extends State<FlowMenu>
+    with SingleTickerProviderStateMixin {
+  late AnimationController menuAnimation;
+
+  void _walkMan(IconData icon) {
+    if (icon == Icons.directions_walk) {
+      print("I'm walk man");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    menuAnimation = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+  }
+
+  @override
+  void dispose() {
+    menuAnimation.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Flow(
+      delegate: FlowMenuDelegate(menuAnimation: menuAnimation),
+      children: <IconData>[
+        Icons.mood,
+        Icons.directions_walk,
+        Icons.attach_money,
+        Icons.sort_outlined,
+      ].map<Widget>(buildItem).toList(),
+    );
+  }
+
+  Widget buildItem(IconData icon) {
+    return SizedBox(
+        width: buttonSize,
+        height: buttonSize,
+        child: FloatingActionButton(
+          elevation: 0,
+          backgroundColor: new Color(0xff2ec4b6),
+          splashColor: Colors.white,
+          child: Icon(
+            icon,
+            color: Colors.white,
+            size: 30,
+          ),
+          onPressed: () {
+            _walkMan(icon);
+            if (menuAnimation.status == AnimationStatus.completed) {
+              menuAnimation.reverse();
+            } else {
+              menuAnimation.forward();
+            }
+          },
+        ));
+  }
+}
+
+class FlowMenuDelegate extends FlowDelegate {
+  final Animation<double> menuAnimation;
+
+  const FlowMenuDelegate({required this.menuAnimation})
+      : super(repaint: menuAnimation);
+
+  @override
+  bool shouldRepaint(FlowMenuDelegate oldDelegate) {
+    return menuAnimation != oldDelegate.menuAnimation;
+  }
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    final size = context.size;
+    final xStart = size.width - buttonSize;
+    final yStart = size.height - buttonSize;
+
+    final n = context.childCount;
+
+    for (int i = 0; i < n; i++) {
+      final isLastItem = i == context.childCount - 1;
+      final setValue = (value) => isLastItem ? 0.0 : value;
+
+      final radius = 180 * menuAnimation.value;
+
+      final theta = i * pi * 0.5 / (n - 2);
+      final x = xStart - setValue(radius * cos(theta));
+      final y = yStart - setValue(radius * sin(theta));
+
+      context.paintChild(
+        i,
+        transform: Matrix4.identity()
+          ..translate(x, y, 0)
+          ..translate(buttonSize / 2, buttonSize / 2)
+          ..rotateZ(
+              isLastItem ? 0.0 : 180 * (1 - menuAnimation.value) * pi / 180)
+          ..scale(isLastItem ? 1.0 : max(menuAnimation.value, 0.5))
+          ..translate(-buttonSize / 2, -buttonSize / 2),
+      );
+    }
+  }
+}
--- odensenightlife/lib/screens/locationListScreen/locationListScreen.dart
+++ odensenightlife/lib/screens/locationListScreen/locationListScreen.dart
@@ -3,6 +3,7 @@ import './locationList.dart';
 import '../../data/argumentModels.dart';
 import 'package:location/location.dart';
 import '../../data/place_dao.dart';
+import 'flowButton.dart';
 
 class LocationListScreen extends StatefulWidget {
   final PlaceDao placeDao = new PlaceDao();
@@ -40,24 +41,26 @@ class _LocationListScreenState extends State<LocationListScreen> {
         ModalRoute.of(context)!.settings.arguments as LocationListArguments;
 
     return Scaffold(
-        backgroundColor: Color(0xff385f71),
-        appBar: AppBar(
-          title: Text(args.descriptiveName),
-          backgroundColor: new Color(0xff2ec4b6),
-        ),
-        body: FutureBuilder<LocationData>(
-            future: getLocation(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.data != null) {
-                  return LocationList(
-                      type: args.type, locationData: snapshot.data!);
-                } else {
-                  return Text("Couldnt get location");
-                }
+      backgroundColor: Color(0xff385f71),
+      appBar: AppBar(
+        title: Text(args.descriptiveName),
+        backgroundColor: new Color(0xff2ec4b6),
+      ),
+      body: FutureBuilder<LocationData>(
+          future: getLocation(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.data != null) {
+                return LocationList(
+                    type: args.type, locationData: snapshot.data!);
               } else {
-                return Center(child: CircularProgressIndicator());
+                return Text("Couldnt get location");
               }
-            }));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
+      floatingActionButton: FlowMenu(),
+    );
   }
 }
